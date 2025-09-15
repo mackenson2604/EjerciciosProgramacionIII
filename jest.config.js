e7943ccd76b429607bbb60b28bc25547bfd81be4
Daniel Parra <dparra@itla.edu.do>
2025-09-15 18:57:35 +0000

    Mejora de los test y retroalimentación

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.js'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
   transform: {
     '^.+\\.js$': 'babel-jest',
   },
